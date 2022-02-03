461cbd53b9dc76bda7e4d65e32d69226132a7c21
capvor <capvor@sina.com>
2022-02-03 20:15:43 +0800

    Remove 8.0 files and Change to 9.0

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-server_version=8.0.7
+server_version=9.0.2
 server_image=customized/seafile-mc:${server_version}
 
 
 mkdir -p ./docker_build
-cp -rf ./image/seafile_8.0/* ./docker_build/
-cp -rf ./scripts_8.0 ./docker_build/
+cp -rf ./image/seafile_9.0/* ./docker_build/
+cp -rf ./scripts_9.0 ./docker_build/
 cd docker_build
 sed -i "s/SEAFILE_VERSION=/SEAFILE_VERSION=${server_version}/" Dockerfile
 docker build -t ${server_image} .
